0622380a6dd0536a249154ae6a8c0f3a1320dbcb
Ollie <oliver.lowman@student.adelaide.edu.au>
2026-03-18 17:35:42 +1030

    added nodemon

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Docker app!');
+  res.end('Hello from Docker app yay!');
 });
 
 server.listen(port, () => {
